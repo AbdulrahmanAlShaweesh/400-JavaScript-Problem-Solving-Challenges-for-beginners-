13a2ff57d3f8eeebfeb67fd7b9e44ce4748a837b
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-01-23 21:33:22 +0300

    Sum Without The Highest And The Lowest Number

--- 15.SumWithoutTheHighestAndTheLowestNumber/sumNumbers.js
+++ 15.SumWithoutTheHighestAndTheLowestNumber/sumNumbers.js
@@ -2,18 +2,15 @@
 // get the heighest number
 // get the lowest number
 // sum all numbers.
-function sumNumber(numbers) {
-  numbers.length === 0 ? 0 : sumNumbers(numbers);
-}
-
-sumNumber([12, -2, 4, 32, 100, 43]);
 
 function sumNumbers(numbers) {
-  let maxNumber = Math.max(...numbers);
-  let minNumber = Math.min(...numbers);
-
-  let filterValue = numbers.filter((x) => x !== maxNumber && x !== minNumber);
-  let newArray = filterValue.reduce((acc, current) => acc + current, 0);
-
-  console.log(newArray);
+  numbers == null
+    ? console.log(0)
+    : console.log(
+        numbers
+          .sort((a, b) => a - b)
+          .slice(1, -1)
+          .reduce((acc, current) => acc + current, 0)
+      );
 }
+sumNumbers([12, -2, 4, 32, 100, 43]);
