09f72b498ca0be494189eb5527dbacbaee59f675
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-01-23 17:17:30 +0300

    complete even old problem

--- 14.CheckEvenOldNumbers/even-odd.js
+++ 14.CheckEvenOldNumbers/even-odd.js
@@ -1,6 +1,27 @@
-
-
-//  this function check if user enter even or odd number. 
-function evenOddNumber() {
-    document.getElementById("user-input").innerHTML; 
+//  this function check if user enter even or odd number.
+// using function and if statements.
+function evenOddNumber(value) {
+  if (value % 2 == 0) {
+    document.getElementById(
+      "user-input"
+    ).innerHTML = `The number ${value} is even number`;
+  } else {
+    document.getElementById(
+      "user-input"
+    ).innerHTML = `The number ${value} is odd number`;
+  }
 }
+
+// -------------------------------------------------------------
+
+// evenOddNumber(11);
+// Other way using terniery operators.
+function evenOddNumbers(number) {
+  number % 2 == 0
+    ? (document.getElementById(
+        "user-input"
+      ).innerHTML = `The number ${number} is even number`)
+    : `The number ${number} is even number`;
+}
+
+evenOddNumbers(12)
