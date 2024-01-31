f1d79ffc0bfbe1e836ba475b3346ffa6ea1f38a0
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-01-31 19:32:36 +0300

    solved the task with two more ways using map and filter functions

--- CountingSheep/countingSheep.js
+++ CountingSheep/countingSheep.js
@@ -4,7 +4,6 @@
 // check if the element is true
 // increase the intial value by 1
 function countingSheep(sheeps) {
-  
   let counter = 0;
   for (let i = 0; i < sheeps.length; i++) {
     sheeps[i] === true ? counter++ : "";
@@ -13,19 +12,38 @@ function countingSheep(sheeps) {
 }
 
 const sheeps = [
-    // true  => sheep move, false => sheep stand at its place;
-    true,
-    true,
-    true,
-    false,
-    true,
-    false,
-    false,
-    false,
-    true,
-    true,
-    false,
-    true,
-  ];
+  // true  => sheep move, false => sheep stand at its place;
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  false,
+  false,
+  true,
+  true,
+  false,
+  true,
+];
 console.log(countingSheep(sheeps));
 
+// doing the task using map function
+function countingSheeps(sheeps) {
+  let counter = 0;
+  sheeps.map((sheep) => (sheep === true ? counter++ : ""));
+
+  return counter;
+}
+
+console.log(countingSheeps(sheeps));
+
+function Sheeps(sheep) {
+  let counter = 0;
+
+  sheep.filter((value, index) => (value === true ? counter++ : ""));
+
+  return counter;
+}
+
+console.log(Sheeps(sheeps));
