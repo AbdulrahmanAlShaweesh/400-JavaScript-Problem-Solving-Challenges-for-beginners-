b441156b2f7f9ef40d7ada1cf0b261690d43bccc
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-01-31 17:20:20 +0300

    adding repeat function to do the same task

--- repeat_string/repeat_str.js
+++ repeat_string/repeat_str.js
@@ -1,14 +1,19 @@
-// create inital string 
-// create loop to loop using number of text need to repeat 
-// add the disert text to the inital string. 
+// create inital string
+// create loop to loop using number of text need to repeat
+// add the disert text to the inital string.
 function repeatString(text, n) {
-  let inttext = ''; 
-  for (let i = 0; i < n; i++) {
-    inttext += text; 
-  }
-  console.log(inttext)
+    let inttext = '';
+    for (let i = 0; i < n; i++) {
+      inttext += text;
+    }
+    console.log(inttext)
 }
 
-repeatString('Hello', 4)
+repeatString("Hello", 4);
 
- 
+// doing the same task using repeat function 
+function repeatText(n, text) {
+    return console.log(text.repeat(n));
+}
+
+console.log(repeatText(10, 'Abdulrahman'))
