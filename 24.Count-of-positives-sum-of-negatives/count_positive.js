4435e1d4d8d0ba03c7a1fa90f3fca5c7c40debaf
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-15 18:39:33 +0300

    adding other soultion using filter and reduce methods

--- 24.Count-of-positives-sum-of-negatives/count_positive.js
+++ 24.Count-of-positives-sum-of-negatives/count_positive.js
@@ -1,8 +1,7 @@
-
-// create function 
+// create function
 // create an empty arry to store the positive numbers
-// create a varable to store the negative numbers. 
-// loop through the array. 
+// create a varable to store the negative numbers.
+// loop through the array.
 // check if the number is positive add it to the array
 // if the number if negative add the negative value to the negative variable
 // return a list contain the number of poistive in the array and the total negative numbers.
@@ -39,5 +38,21 @@ console.log(
     -15,
     2,
     -11,
+  ])
+);
+
+// --------------------------------------- using build in function ----------------------------------------- //
+function countPoitives(array) {
+  return [
+    array.filter((positive) => positive > 0).length,
+    array
+      .filter((negativeNumber) => negativeNumber < 0)
+      .reduce((x, y) => x + y, 0),
+  ];
+}
+
+console.log(
+  countPoitives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15, 2, -11,
   ])
 );
