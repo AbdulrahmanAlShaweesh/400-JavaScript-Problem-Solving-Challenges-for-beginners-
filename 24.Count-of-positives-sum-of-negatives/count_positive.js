06a1123ad180ce5b409c56f6ef782efb77277361
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-15 17:45:13 +0300

    count of positives sum of negatives

--- 24.Count-of-positives-sum-of-negatives/count_positive.js
+++ 24.Count-of-positives-sum-of-negatives/count_positive.js
@@ -1,3 +1,36 @@
 function countPositive(array) {
-    
+  let counter = 0;
+  negative_value = 0;
+  var result = [];
+
+  for (var element in array) {
+    array[element] < 0 ? negative_value += array[element] : counter += 1;
+  }
+  result.push(counter);
+  result.push(negative_value);
+
+  return result;
 }
+
+console.log(
+  countPositive([
+    1,
+    ,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    -11,
+    -12,
+    -13,
+    -14,
+    -15,
+    2,
+    -11,
+  ])
+);
