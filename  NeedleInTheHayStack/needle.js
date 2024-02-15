b3fae45aadef75a357a8d5385a81a72691dd9a76
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-15 17:27:14 +0300

    the proble solved with index of

---  NeedleInTheHayStack/needle.js
+++  NeedleInTheHayStack/needle.js
@@ -1,11 +1,38 @@
-// check if the word needle is in the array, if yes then will return the needle word pluse the index 
-// where the word found. 
+// check if the word needle is in the array, if yes then will return the needle word pluse the index
+// where the word found.
 function FindNeedle(hayStack) {
   for (var i in hayStack) {
-    if (hayStack[i] == "needle") {
-      console.log(`needle in index ${i}`);
+    if (hayStack[i] === "needle") {
+      console.log(`needle found in Position: ${i}`);
     }
   }
 }
 
-FindNeedle(["mohammed", "mahmoud", "sager", "needle"]);
+FindNeedle([
+  "mohammed",
+  "mahmoud",
+  "sager",
+  "hay",
+  "jack",
+  "hay",
+  "needle",
+  "randomjack",
+]);
+
+// other way using indexof.
+function needle(hayStack) {
+  return `needle found in position: ${hayStack.indexOf("needle")}`;
+}
+
+console.log(
+  needle([
+    "mohammed",
+    "mahmoud",
+    "sager",
+    "hay",
+    "jack",
+    "hay",
+    "needle",
+    "randomjack",
+  ])
+);
