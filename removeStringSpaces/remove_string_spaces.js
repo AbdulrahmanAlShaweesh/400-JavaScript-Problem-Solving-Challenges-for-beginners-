e388e316c4d0ca0bc2c54a9fb119b7daf3064659
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-18 09:51:43 +0300

    complete the task

---
+++ removeStringSpaces/remove_string_spaces.js
@@ -0,0 +1,24 @@
+// get the string from using
+// loop through the string.
+// check if the letter is a space : remove is
+// else added to inital strnning
+// return the stirng
+function removeString(text) {
+  let initalText = "";
+
+  for (let i in text) {
+    text[i] === " " ? "" : (initalText += text[i]);
+  }
+  return initalText;
+}
+
+console.log(removeString("Abd lrah      man"));
+
+// other way to solve the problem.
+// get the string from user.
+// using repace method to change the white space into nothing.
+function removeWhiteSpace(str) {
+  return str.replace(" ", ""); // remove the white space and replace it with nothing.
+}
+
+console.log(removeWhiteSpace("Aadadsasd asdasd"));
