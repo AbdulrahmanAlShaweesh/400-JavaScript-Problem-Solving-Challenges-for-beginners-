e3cd7fe42058045ed8d22f8d4392da6164f5a5f4
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-19 14:49:17 +0300

    completed the task

---
+++ 37. welcome-audionce/wecome_aud.js
@@ -0,0 +1,25 @@
+function welcomeAudiance(country) {
+  let languages = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Brnvenuto",
+    latvian: "Gaidits",
+    lithanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+
+  return languages.hasOwnProperty(country) ? languages[country] : "Welcome";
+}
+
+console.log(welcomeAudiance("spanish"));
