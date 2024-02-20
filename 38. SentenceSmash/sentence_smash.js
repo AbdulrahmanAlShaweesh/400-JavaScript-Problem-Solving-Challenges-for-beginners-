1edba8a45376eebc945f9cf27931f85d9b2f0740
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-20 11:14:57 +0300

    completed the task using join method

---
+++ 38. SentenceSmash/sentence_smash.js
@@ -0,0 +1,10 @@
+// get an array of words
+// use join to return a string
+// add a space between the words
+// return the smashed sentance
+function sentenceSmashed(array) {
+  return array.join(' ').trim();
+}
+console.log(sentenceSmashed(["Hello", "World", "is", "best", "place", "to", "leave", "in"]))
+
+// other way using
