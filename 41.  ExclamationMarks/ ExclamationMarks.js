4d08ecc43a59d079f02cb5c914d83101362ed85b
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-20 12:37:07 +0300

    completed the task

--- 41.  ExclamationMarks/ ExclamationMarks.js
+++ 41.  ExclamationMarks/ ExclamationMarks.js
@@ -1,12 +1,10 @@
-
-
 // get the string from user
 // check if the end content exclamation Mark  using slice(-1) method
 // remove it and return the rest
-// else return the sentence 
+// else return the sentence
 function remove(string) {
-    // return string.slice(-1)
-    return string.slice(-1) === '!'? string.slice(0, string.length-1): string;
+  // return string.slice(-1)
+  return string.slice(-1) === "!" ? string.slice(0, string.length - 1) : string;
 }
 
-console.log(remove('!mohammed!'))
+console.log(remove("!mohammed!"));
