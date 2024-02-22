79c0e294fbe7adf6d81fed37d772eefcf50ba961
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-22 16:04:02 +0300

    compleded the task using map and terneray operators

---
+++ 43. RemovingElements/removing_elements.js
@@ -0,0 +1,16 @@
+// get an array
+// create an empty array
+// loop through old array
+// check if the index of the element is even
+// add element to new array
+// elese do nothing
+function removingElements(array) {
+  let newArray = [];
+
+  array.map((str, i) => (i % 2 === 0 ? newArray.push(array[i]) : ""));
+  return newArray;
+}
+
+console.log(
+  removingElements(["keep", "remove", "keep", "remove", "keep", "..."])
+);
