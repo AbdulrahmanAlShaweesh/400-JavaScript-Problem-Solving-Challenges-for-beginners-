666cdaf62fa74274a87033b8abd474343f10e75c
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-22 15:54:03 +0300

    compleded

--- 42. AreYouPlayingBanjo/are_you_playing_banjo.js
+++ 42. AreYouPlayingBanjo/are_you_playing_banjo.js
@@ -1,12 +1,13 @@
-
-// get the name 
-// get the first letter and use uppercase 
-// check if the first letter is r or R. 
-// return name playing bunjo 
-// else return name does not playing bungo. 
+// get the name
+// get the first letter and use uppercase
+// check if the first letter is r or R.
+// return name playing bunjo
+// else return name does not playing bungo.
 
 function areYouPlayingBanjo(name) {
-    return name.slice(0, 1).toUpperCase() === 'R'? `${name} is playing Bunjo`: `${name} does not playing Bungo`;
+  return name[0].toUpperCase() === "R"
+    ? `${name} is playing Bunjo`
+    : `${name} does not playing Bungo`;
 }
 
-console.log(areYouPlayingBanjo('muahhmed'))
+console.log(areYouPlayingBanjo("muahhmed"));
