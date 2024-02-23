ede9c474c505eb670cf761be7de98136b05e6362
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-23 11:04:11 +0300

    completed the task

---
+++ 49. FormattingToDecimalPlaces/Formatting_To_Decimal_Places.js
@@ -0,0 +1,9 @@
+// get a decimal number.
+// round it to 2 decima places using toFixed(2) method.
+// return the result.
+
+function roundTwoDecimalPlaces(n) {
+    return n.toFixed(2)    // return a number with two decimal places.
+}
+
+console.log(roundTwoDecimalPlaces(11.443))
