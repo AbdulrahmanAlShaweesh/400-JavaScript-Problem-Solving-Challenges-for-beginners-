d16fe74b74e8df58ef1fb5bef9b37ee1f48be1dc
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-25 15:37:23 +0300

    added a new function to solve the task using for loop

--- 54.  GetTheMean OfAnArray/ get _the_mean_of_an_array.js
+++ 54.  GetTheMean OfAnArray/ get _the_mean_of_an_array.js
@@ -4,8 +4,20 @@
 // divide the totl with the total of subjects
 // return the mean.
 function getMean(marks) {
-  let total = 0;
   return marks.reduce((n1, n2, i) => n1 + n2) / marks.length;
 }
 
 console.log(getMean([1, 2, 3, 4]));
+
+// way 2 : using loop
+
+function gettingMean(marks) {
+  let total = 0;
+
+  for (let i in marks) {
+    total += marks[i];
+  }
+  return total / marks.length; 
+}
+
+console.log(gettingMean([1, 2, 3, 4]))
