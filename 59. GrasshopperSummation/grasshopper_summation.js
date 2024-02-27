af64f90469ee7c0d1d6efd15334661fbb439adc7
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-28 00:40:18 +0300

    completed the task using for loop

---
+++ 59. GrasshopperSummation/grasshopper_summation.js
@@ -0,0 +1,17 @@
+// get a number. 
+// create an intial varable with 0 value
+// loop through the number
+// each time add the current number to the inital value
+// .retunr the value 
+function GrasshopperSummation(number) { 
+    let sum = 0; 
+
+    for (let i = 1; i <= number; i++) {
+        sum += i; 
+    }
+
+    return sum; 
+}
+// 3 == 1 + 2 => 3
+// 8 == 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 => 36
+console.log(GrasshopperSummation(8))
