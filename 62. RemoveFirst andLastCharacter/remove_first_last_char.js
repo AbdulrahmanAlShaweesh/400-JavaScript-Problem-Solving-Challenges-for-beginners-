a70f03e17caa65dd7483da301fb5c93a213ed661
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-28 14:47:27 +0300

    task completed

---
+++ 62. RemoveFirst andLastCharacter/remove_first_last_char.js
@@ -0,0 +1,9 @@
+// getting a string. 
+// remove the first and last char using indexof
+// return the rest of the string.
+
+function removeFirstLastChar(string) {
+   return string.slice(1, -1);
+}
+
+console.log(removeFirstLastChar('Abdulrahman'))
