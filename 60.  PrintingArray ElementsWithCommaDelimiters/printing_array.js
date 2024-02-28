1be453e39d849072b08298a6921826c631619d10
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-28 13:38:52 +0300

    added a new function to solve the task using for loop and slice method

--- 60.  PrintingArray ElementsWithCommaDelimiters/printing_array.js
+++ 60.  PrintingArray ElementsWithCommaDelimiters/printing_array.js
@@ -8,3 +8,16 @@ function printtingArray(array) {
 
 console.log(printtingArray(["h", "e", "l", "l", "o"]));
 console.log(printtingArray(["h", "o", "l", "a"])); // "h,o,l,a "
+
+// other way to do the task.
+function printArray(array) {
+  let string = "";
+
+  for (let i = 0; i < array.length; i++) {
+    string +=  array[i] + ',';
+  }
+
+  return `"${string.slice(0, -1)}"`;
+}
+
+console.log(printArray(["h", "o", "l", "a"]));
