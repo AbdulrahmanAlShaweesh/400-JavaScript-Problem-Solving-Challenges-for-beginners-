d9dc2f75f8db6e98c249b6535126da723454192e
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-03-07 14:51:27 +0300

    completed the task using map

---
+++ 66. BeginnerLost WithoutMap/beginner_lost_without_map.js
@@ -0,0 +1,10 @@
+// get an array of a numbers.
+// loop through the array.
+// muliply the number with itself.
+// added the number to a new array
+// return the new array
+function maps(numbers) {
+  return numbers.map((number) => number + number);
+}
+
+console.log(maps([1, 2, 3]));
